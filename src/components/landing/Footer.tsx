0f7e95c2a8dcea4be895fd9354cc89cdae151e92
Jose Saicoo <josesaicoo20@gmail.com>
2025-11-04 17:51:11 +0000

    ponlo a 40 pixeles

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
               <img
                 src="https://i.ibb.co/3YfRRdwr/sign-ai-logo.png"
                 alt="SignAI Logo"
-                width={100}
+                width="100"
               />
             </Link>
           </div>
--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
           <img
             src="https://i.ibb.co/3YfRRdwr/sign-ai-logo.png"
             alt="SignAI Logo"
-            width={100}
+            width="100"
           />
         </Link>
 
@@ -65,7 +65,7 @@ export function Header() {
                   <img
                     src="https://i.ibb.co/3YfRRdwr/sign-ai-logo.png"
                     alt="SignAI Logo"
-                    width={100}
+                    width="100"
                   />
               </Link>
               <div className="flex flex-col space-y-3">
